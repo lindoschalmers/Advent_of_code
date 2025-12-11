760cbe6e19bec279580598e3a2cf7c6d79b76eed
lindoschalmers <lindos@chalmers.se>
2025-12-11 01:38:32 +0100

    part 1 day 2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,16 +1,88 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+long long int globalSum = 0;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+void add_sum (long int num) {
+    globalSum += num;
+}
+
+long int find_length(long int length) {
+    int i = 0;
+    while (length > 0) {
+        length = length/10;
+        i++;
     }
+    length = i;
+    return length;
+}
+
+int check_range(long int min, long int max) {
+    printf("Checking range: %ld - %ld\n", min, max);
+    long int length = 0;
+    for (long int i = min; i <= max; i++) {
+       // printf("%d\n", i);
+        length = find_length(i);
+        //printf("Length: %ld\n", length);
+        long int temp = i;
+        long int arr[length];
+        if (length % 2 == 0) {
+            for (int j = 0; j < length; j++) {
+                arr[length-1-j] = temp % 10;
+                temp = temp/10;
+            }
+           long int sum1 = 0, sum2 = 0;
+            for (int j = 0; j < length; j++) {
+                if (j >= length/2) {
+                    sum2 = sum2*10 + arr[j];
+                }
+                else {
+                    sum1 = sum1*10 + arr[j];
+                }
+            }
+            if (sum1 == sum2) {
+               // printf("sum1: %d, sum2: %d, i: %d\n", sum1, sum2, i);
+                add_sum(i);
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    std::ifstream file("values.txt");
+        if (!file.is_open()) {
+            std::cerr<< "Can't open file" << std::endl;
+            return 1;
+        }
+    char ch;
+    long int num1 = 0, num2 = 0, x, i = 0;
+    while (file.get(ch)) {
+        if (ch == '-') {
+            while (file.get(ch)) {
+                if (ch == ',' || ch == '\n') {
+                    check_range(num1, num2);
+                    i = 1;
+                    break;
+                }
+                if (ch != ' ' && ch != ',') {
+                    x = ch - '0';
+                    num2 = num2 * 10 + x;
+                }
+            }
+        }
+        if (i == 1) {
+            num1 = 0;
+            num2 = 0;
+            i = 0;
+        }
+        if (ch != ',' && ch != '-' && ch != ' ') {
+            x = ch - '0';
+            num1 = num1 * 10 + x;
+        }
+    }
+    printf("Global Sum: %lld\n", globalSum);
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
